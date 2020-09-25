0704a22be9a424c42edef2b5e20c96b055779e4c
Tasfik Rahman <tasfikrahman001@gmail.com>
2020-09-25 12:14:10 +0600

    k-subarray-avg [sliding window]

--- Solutions of Grokking Interview Questions (Educative.io)/Pattern Sliding/k-subarray-avg.cpp
+++ Solutions of Grokking Interview Questions (Educative.io)/Pattern Sliding/k-subarray-avg.cpp
@@ -19,13 +19,31 @@ vector<double> subArrayAvg(int *arr, int n, int k)
     }
     return ans;
 }
+// sliding window | T -> O(n)
+vector<double> subArrayAvg2(int *arr, int n, int k)
+{
+    vector<double> ans(n - k + 1);
+    double sum = 0;
+    int windowStart = 0;
+    for (int windowEnd = 0; windowEnd < n; windowEnd++)
+    {
+        sum += arr[windowEnd];
+        if (windowEnd >= k - 1)
+        {
+            ans[windowStart] = (double)sum / k;
+            sum -= arr[windowStart++];
+        }
+    }
+    return ans;
+}
 
 int main()
 {
     int arr[] = {1, 3, 2, 6, -1, 4, 1, 8, 2};
     int n = sizeof(arr) / sizeof(int);
     int k = 5;
-    vector<double> ans = subArrayAvg(arr, n, k);
+    //vector<double> ans = subArrayAvg(arr, n, k);
+    vector<double> ans = subArrayAvg2(arr, n, k);
     for (double avg : ans)
     {
         cout << avg << ", ";
